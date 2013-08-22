13c04267c276ba3f968ce56a648fbc8d328dc3d6
andydenenberg <andydenenberg@gmail.com>
2013-08-22 12:18:41 -0500

    more realtime graphing

--- app/assets/javascripts/controllers/home_controller.js
+++ app/assets/javascripts/controllers/home_controller.js
@@ -1,35 +1,44 @@
 HelloEmber.HomeController = Ember.ArrayController.extend({
-	content: [  ],
 	needs: ['Graph', 'Portfolios'],
-	timer_count: 60,
+	series: [  ],
+	tv_count: 0,
+	
+  	count: function() {
+	return this.series.length  
+  		}.property('series'),
+	
+	timer_count: 15,
+	most_recent: 0,
 	
 	load_graph: function(){
+		this.update_content() ;
+		var GraphController = this.get('controllers.Graph');
+		GraphController.load_data( 'total_combined_value', 'Total Combined Value', '08:00AM', '04:00PM', '60 minutes', [this.series] );
+	},	
+	
+	update_content: function() {
 		var PortfoliosController = this.get('controllers.Portfolios')
-		var content = this.get('content')
+
 		var now = new Date();
 		var h = now.getHours();
-		var ap = (h > 12) ? 'PM' : 'AM'
+		var ap = (h > 11) ? 'PM' : 'AM'
 		if (ap == 'PM') { h = h - 12 }
 		var m = now.getMinutes() + '' ; // convert to string
-	    while (m.length < 2) m = "0" + m; 
-	    
+	    while (m.length < 2) m = "0" + m; 	    
 		var new_time = h + ':' + m + ap ;
-		
-//		var tv = PortfoliosController.get('total_value') ;
-		tv = HelloEmber.Portfolio.find().reduce(function(accum, portfolio) { 
-	                      return accum + portfolio.get('portfolio_value') }, 0) ;
-		content.push([new_time, tv ]) ;
-		this.set('content', content) ;
-		var GraphController = this.get('controllers.Graph');
-//		GraphController.set('portfolio_names', [ this.portfolio_select ] ) ;
-		GraphController.load_data( 'total_combined_value', 'Total Combined Value', '08:00AM', '04:00PM', '60 minutes', [this.content] );
-	},	
+
+		var series = this.get('series')
+		this.set('most_recent', HelloEmber.Portfolio.find().reduce(function(accum, portfolio) { 
+	                      return accum + portfolio.get('portfolio_value') }, 0) );
+		series.push([new_time, this.get('most_recent') ]) ;
+		this.set('series', series) ;
+		this.set('tv_count', this.series.length) ;
+	},
 
 	timer_update: function() {
 		var timer_count = this.timer_count - 1 ;
-		console.log(timer_count) ;
 		if (timer_count <= 0 ) {
-			this.load_graph() ;
+			this.update_content() ;
 			timer_count = 60 ;
 		}
 		this.set('timer_count', timer_count ) ;
--- app/assets/javascripts/routes/home_route.js
+++ app/assets/javascripts/routes/home_route.js
@@ -1,1 +1,7 @@
-HelloEmber.HomeRoute = HelloEmber.AuthenticatedRoute.extend({ });
+HelloEmber.HomeRoute = HelloEmber.AuthenticatedRoute.extend({ 
+	
+	setupController: function(controller, model) {
+//	controller.load_graph()  
+	},
+  
+});
